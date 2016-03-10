f22b4224fffcf863a79b9eed2ee044e7ba76ed8e
Krunoslav Husak <krunoslav.husak@skole.hr>
2016-03-10 13:46:51 +0100

    Ispis boje ovisno o spolu.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Naslov</title>
 </head>
 <body>
@@ -9,17 +10,45 @@
 
 <p>
 
-<form>
+<form method="post">
     Ime:<br>
     <input type="text" name="ime"><br>
     Prezime:<br>
     <input type="text" name="prezime"><br>
+    Spol:<br>
+    <input type="radio" name="spol" value="M" checked>Muško<br>
+    <input type="radio" name="spol" value="Ž">Žensko<br>
     <input type="submit" value="Idu dani!">
+
 </form>
 
 <?php
-echo('Vaše ime je: ' . htmlspecialchars($_GET['ime']) . '<br>');
-echo('Vaše prezime je: ' .$_GET['prezime']);
+if ($_POST['spol'] == 'M')
+{
+    $class = 'musko';
+}
+elseif ($_POST['spol'] == 'Ž')
+{
+    $class = 'zensko';
+}
+
+echo('<p class="' . $class .'">Vaše ime je: ' . htmlspecialchars($_POST['ime']) . '</p>');
+echo('<p class="' . $class .'">Vaše prezime je: ' . htmlspecialchars($_POST['prezime']) . '</p>');
+
+echo('<br>');
+
+if ($_POST['spol'] == 'M')
+{
+    echo('Vi ste pravi muškarac!');
+}
+elseif ($_POST['spol'] == 'Ž')
+{
+    echo('Vi ste prava žena!');
+}
+else
+{
+    echo('A što ste onda?');
+}
 ?>
 
 </p>
